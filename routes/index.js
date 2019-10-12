d3f971570a68342f2524b9e4dfc2046509f05ba3
rubik16x16 <drt_mike@hotmail.com>
2019-10-12 09:57:37 -0400

    Habilitar cors

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var test = require('../controllers/test');
 var contactos = require('../controllers/contactos');
 
 /* GET home page. */
@@ -13,7 +12,5 @@ router.get('/contactos', contactos.index);
 router.post('/contactos', contactos.store);
 router.put('/contactos/:id', contactos.update);
 router.delete('/contactos/:id', contactos.destroy);
-
-router.get('/pop', test.test);
 
 module.exports = router;
